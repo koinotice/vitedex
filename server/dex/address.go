13ab9ad107c1425ed809161869c71f6a5305f720
koinotice <koinotice@gmail.com>
2019-07-10 18:32:27 +0800

    update name

--- server/dex/address.go
+++ server/dex/address.go
@@ -1,12 +1,14 @@
 package dex
 
 import (
+	"fmt"
 	"github.com/ethereum/go-ethereum/common"
+	eth "github.com/koinotice/vitedex/ethereum"
+	"github.com/koinotice/vitedex/genv/sdk/ethereum"
 	"github.com/koinotice/vitedex/plugin"
+	"github.com/koinotice/vitedex/server/models"
 	"github.com/koinotice/vitedex/structs"
 	"strings"
-	"fmt"
-	eth "github.com/koinotice/vitedex/ethereum"
 )
 type Dex struct {
 
@@ -22,26 +24,39 @@ func parseAddress(addString string) string {
 func Callback(txAndReceipt *structs.RemovableTxAndReceipt) {
 	transferEventSig := "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
 
-	receipt1 := txAndReceipt.Receipt
+	// todo a little weird
+	if receipt, ok := txAndReceipt.Receipt.(*ethereum.EthereumTransactionReceipt); ok {
+		for _, log := range receipt.Logs {
+			if len(log.Topics) == 3 && log.Topics[0] == transferEventSig {
+				//fromAddress ,err:=eth.ParseAddress(log.Topics[1])
+				//if    err!= nil {
+				//	fmt.Printf("%s hex,%s",fromAddress.String(),fromAddress.Hex())
+				//}
+				address:=log.Address
+				hash:=log.TransactionHash
+				from :=parseAddress(log.Topics[1])
+				to :=parseAddress(log.Topics[2])
 
-	for _, log := range receipt1.GetLogs() {
-		topics := log.GetTopics()
-		if len(topics) == 3 && topics[0] == transferEventSig {
 
-			//fmt.Printf(">> approving to dydx, tx: %s\n", txAndReceipt.Tx.GetTo())
-			//fmt.Printf(">> approving to dydx, tx: %s\n", txAndReceipt.Tx.GetFrom())
-			//fmt.Printf(">> approving to dydx, tx: %s\n", txAndReceipt.Tx)
-			amount, ok := plugin.HexToDecimal(log.GetData())
-			if (ok) {
+				amount, ok := plugin.HexToDecimal(log.Data)
 
-				fmt.Printf(">> tx: %s\n,%s\n,%s\n,amount%s\n", txAndReceipt.Tx.GetHash(),
-					txAndReceipt.Tx.GetFrom(), txAndReceipt.Tx.GetTo(), amount)
-				//fmt.Printf(">> approving to dydx, tx: %s\n", amount)
+				if ok {
 
-			} else {
-				fmt.Printf("error %s\n", ok)
+
+					 walletLog := models.WalletDao.FindWalletByErcAddress(to)
+					//
+					if walletLog == nil {
+						//utils.Debugf("Skip useless transaction %s", tx.GetHash())
+
+					} else {
+						fmt.Printf("\nfuck ,hash %s address %s,from %s ,to %s ,amount %s \n",hash,address,from,to,amount)
+
+						//return true
+					}
+					//fmt.Printf("fuck ,hash %s address %s,from %s ,to %s ,amount %s \n",hash,address,from,to,amount)
+					//rst = append(rst, TransferEvent{log.Address, from, to, amount})
+				}
 			}
-
 		}
 	}
 
---
+++ server/models/wallet.go
@@ -0,0 +1,80 @@
+package models
+
+import (
+	//"database/sql"
+	"fmt"
+	"time"
+)
+
+type IWalletDao interface {
+	FindWalletByErcAddress(walletHash string) *Wallet
+	InsertWallet(wallet *Wallet) error
+	UpdateWallet(wallet *Wallet) error
+	Count() int
+	//FindWalletByID(id int64) *Wallet
+}
+
+type Wallet struct {
+	ID          int64  `json:"id"              db:"id" primaryKey:"true"  autoIncrement:"true" gorm:"primary_key"`
+	EthAddress  string `json:"eth_address"        db:"eth_address"`
+	ViteAddress string `json:"vite_address" db:"vite_address"`
+
+	UpdatedAt time.Time `json:"updatedAt"       db:"createdAt"`
+	CreatedAt time.Time `json:"createdAt"       db:"updatedAt"`
+}
+
+func (Wallet) TableName() string {
+	return "wallets"
+}
+
+var WalletDao IWalletDao
+var WalletDaoPG IWalletDao
+
+func init() {
+	WalletDao = &walletDaoPG{}
+	WalletDaoPG = WalletDao
+}
+
+type walletDaoPG struct {
+}
+
+func (walletDaoPG) FindWalletByErcAddress(address string) *Wallet {
+	var wallet Wallet
+
+	fmt.Print("fuck")
+	fmt.Print(address)
+	DB.Where("eth_address = ?", address).First(&wallet)
+
+	if wallet.EthAddress == "" {
+		return nil
+	}
+
+	
+
+	return &wallet
+}
+
+func (walletDaoPG) InsertWallet(wallet *Wallet) error {
+	return DB.Create(wallet).Error
+}
+
+func (walletDaoPG) UpdateWallet(wallet *Wallet) error {
+	return DB.Save(wallet).Error
+}
+
+func (walletDaoPG) Count() int {
+	var count int
+	DB.Model(&Wallet{}).Count(&count)
+	return count
+}
+
+//func (walletDaoPG) FindWalletByID(id int64) *Wallet {
+//	var wallet Wallet
+//
+//	DB.Where("id = ?", id).Find(&wallet)
+//	if wallet.Status == "" {
+//		return nil
+//	}
+//
+//	return &wallet
+//}
